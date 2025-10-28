1f25685b64e6b07cca38561fb704e70aef05fa00
Devin <devin.smith.work@gmail.com>
2025-10-28 12:28:45 -0700

    updates for boto3

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 from datetime import datetime
 import subprocess
-from dotenv import dotenv_values
 import pyodbc
 import csv
+import boto3
+import requests
+
+# Get AWS session
+session = boto3.Session()
 
 
 def main():
-    creds = dotenv_values()
     today_string = datetime.today().strftime('%Y-%m-%d')
 
-    with open('pgd_change_report.sql') as f:
-        pgd_change_query = f.read().replace('#REPLACE', today_string)
+    # Get reporting DB creds
+    reporting_db_creds = get_ssm_parameters(
+        folder="/pub-oapi-tools/elements-reporting-db/prod",
+        names=['driver', 'server', 'database', 'user', 'password'])
 
-    pdg_change_report = query_reporting_db(creds, pgd_change_query)
+    # Get email addresses DB creds
+    emails = get_ssm_parameters(
+        folder="/pub-oapi-tools/emails",
+        names=['devin', 'alainna'])
+
+    # Load the sql file data from Github, replace a text string with today's date
+    pgd_change_query = get_sql_from_github()
+    pgd_change_query = pgd_change_query.replace('#REPLACE', today_string)
+
+    pdg_change_report = query_reporting_db(reporting_db_creds, pgd_change_query)
     pdg_change_report_file = write_csv_file(pdg_change_report, "pdg_change_report", today_string)
 
     # Set up the mail process with attachment and email recipients
     subprocess_setup = ['mail',
                         '-s', 'TEST: PGD changes report',
                         '-a', pdg_change_report_file]
-    subprocess_setup += [creds['DEVIN'], creds['ALAINNA']]
+    subprocess_setup += [emails['devin'], emails['alainna']]
 
     # Text in the email body
     input_byte_string = b'''
-        The attached CSV includes users' primary group changes
-        in the span of one month (for the tests, it's from 2025-05-01)
+        The attached CSV includes users' primary group changes in the span of one month.
+        
+        Future-Proofing: This automated email was sent from the following program:
+        https://github.com/eScholarship/oapi-user-data-changes-report/ 
     '''
 
     # Run the subprocess with EOT input to send
@@ -37,11 +53,11 @@ def main():
 def query_reporting_db(creds, query):
     # Connect to Elements reporting db
     conn = pyodbc.connect(
-        driver=creds['ELEMENTS_REPORTING_DB_DRIVER'],
-        server=(creds['ELEMENTS_REPORTING_DB_SERVER'] + ',' + creds['ELEMENTS_REPORTING_DB_PORT']),
-        database=creds['ELEMENTS_REPORTING_DB_DATABASE'],
-        uid=creds['ELEMENTS_REPORTING_DB_USER'],
-        pwd=creds['ELEMENTS_REPORTING_DB_PASSWORD'],
+        driver=creds['driver'],
+        server=(creds['server'] + ',1433'),
+        database=creds['database'],
+        uid=creds['user'],
+        pwd=creds['password'],
         trustservercertificate='yes')
 
     print(f"Connected to Elements reporting DB, querying.")
@@ -56,6 +72,13 @@ def query_reporting_db(creds, query):
     return rows
 
 
+def get_sql_from_github():
+    github_raw_link = "https://raw.githubusercontent.com/eScholarship/oapi-user-data-changes-report/main/"
+    input_file = "pgd_change_report.sql"
+    response = requests.get(f"{github_raw_link}{input_file}")
+    return response.text
+
+
 def write_csv_file(data, filename, today_string):
     filename_with_date = f"output/{filename}-{today_string}.csv"
     with open(filename_with_date, "w") as outfile:
@@ -65,6 +88,21 @@ def write_csv_file(data, filename, today_string):
         writer.writerows(data)
 
     return filename_with_date
+
+
+# Parses data from parameter store
+def get_ssm_parameters(folder, names):
+    print("Connect to SSM for parameters")
+    ssm_client = session.client(service_name='ssm', region_name='us-west-2')
+
+    param_names = [f"{folder}/{name}" for name in names]
+    response = ssm_client.get_parameters(Names=param_names, WithDecryption=True)
+
+    param_values = {
+        (param['Name'].split('/')[-1]): param['Value']
+        for param in response['Parameters']}
+
+    return param_values
 
 
 # Stub for main
